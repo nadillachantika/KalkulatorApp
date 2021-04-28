f6b10d498eb1224f5153f1a29c2839a507a734af
Nadilla <nadilla.c.putri@gmail.com>
2021-04-28 11:30:41 +0700

    first commit

---
+++ app/src/main/java/com/nadillla/week2/KalkulatorActivity.kt
@@ -0,0 +1,11 @@
+package com.nadillla.week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KalkulatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kalkulator)
+    }
+}
